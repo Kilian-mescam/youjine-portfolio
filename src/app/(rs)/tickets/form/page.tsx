bf197422d0d80a38cfefe078da23eaebf7567151
Kilian <kmescam@gmail.com>
2024-12-05 18:21:27 +0100

    ticket form

--- src/app/(rs)/tickets/form/page.tsx
+++ src/app/(rs)/tickets/form/page.tsx
@@ -2,6 +2,7 @@ import { BackButton } from "@/components/ui/BackButton";
 import { getCustomer } from "@/lib/queries/getCustomer";
 import { getTicket } from "@/lib/queries/getTicket";
 import * as Sentry from "@sentry/nextjs"
+import TicketForm from "./TicketForm";
 
 export default async function TicketFormPage({
     searchParams, 
@@ -42,7 +43,9 @@ export default async function TicketFormPage({
                 )
             }
 
-            // return ticket form
+            // return ticket form 
+            console.log(customer)
+            return <TicketForm customer={customer} />
 
         }
 
@@ -64,7 +67,7 @@ export default async function TicketFormPage({
             // return ticket form 
             console.log('ticket: ', ticket)
             console.log('customer: ', customer)
-
+            return <TicketForm customer={customer} ticket={ticket} />
         }
     } catch (e) {
         if (e instanceof Error) {
